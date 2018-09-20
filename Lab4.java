044d16604a0bf27d328b7286deb644e89c01a0f9
barneshn <43420086+barneshn@users.noreply.github.com>
2018-09-20 10:53:07 -0400

    Update Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -1,3 +1,4 @@
+package Lab_3_Git;
 import java.util.Collections;
 import java.util.Arrays;
 
@@ -11,28 +12,42 @@ public class Lab4 {
         }
 
         //sort array method
-        sort(array);
-
-
+        sortDecending(array);
 
 
     }
 
-    public static int[] sort(int[] array){
+    public static int[] sortDecending(int[] array) {
         //Create new empty array
         int[] array2 = new int[array.length];
 
         //Copy array to array2
-        System.arraycopy( array, 0, array2, 0, array.length );
+        System.arraycopy(array, 0, array2, 0, array.length);
 
-        System.out.println("Copied & sorted array");
+        int temp;
+        // cycle through array
         for (int i = 0; i < array2.length; i++) {
-            System.out.println(+ array2[i] + " ");
+            // cycle through array values + 1 (this way we see the first 2 array values as (i,j))
+            for (int j = i + 1; j < array2.length; j++) {
+                //compare i to j, when j is bigger save the i value as temp and replace i with j. set temp = j.
+                if (array2[i] < array2[j])
+                {
+                    temp = array2[i];
+                    array2[i] = array2[j];
+                    array2[j] = temp;
+                }
+            }
         }
 
-        Arrays.sort(array2);
-        
+        System.out.println("Copied & sorted in decending order");
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println(+array2[i] + " ");
+        }
+
+
         return array2;
         //return null;
     }
 }
+
+
